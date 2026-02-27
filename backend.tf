52a3489ff46ef896467983fac59d5d5850e5f2d7
joyobida <ochanyajoy2@gmail.com>
2026-02-26 20:23:37 -0600

    remove real bucket name

--- backend.tf
+++ backend.tf
@@ -1,7 +1,7 @@
 
 terraform {
   backend "s3" {
-    bucket = "ochie-terraformbucket"
+    bucket = "replace here with bucket name"
     key = "alb/terraform.state"
     region = "us-east-1"
     encrypt = true
